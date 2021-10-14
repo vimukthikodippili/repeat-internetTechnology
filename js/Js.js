2e82570f96ee32441d02a727b1dddc427daf27d9
unknown <you@example.com>
2021-10-14 17:39:09 +0530

    done

---
+++ js/Js.js
@@ -0,0 +1,6 @@
+document.getElementById("myBtn").onclick = function() {myFunction()};
+
+/* myFunction toggles between adding and removing the show class, which is used to hide and show the dropdown content */
+function myFunction() {
+    document.getElementById("myDropdown").classList.toggle("show");
+}
